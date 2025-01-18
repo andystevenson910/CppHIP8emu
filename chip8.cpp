d98267cfdb05ea11e42c760aae7760775b6fd46b
Andy <andystevenson910@gmail.com>
2025-01-17 18:07:43 -0700

    basic setup implemented

--- chip8.cpp
+++ chip8.cpp
@@ -1,4 +1,6 @@
 #include <cstdint>
+#include <cstack>
+
 class chip8{
     private:
         bool display[32][64] = {};
@@ -8,5 +10,17 @@ class chip8{
         //12 bits wide the first nibble is unused maybe enforce that later
         uint16_t addressRegister = 0;
 
+        uint8_t stack[64] = {};
+
+        uint8_t stackPointer = 0;
+
+        uint8_t delayTimer = 0;
+        uint8_t soundTimer = 0;
+
+        uint16_t programCounter = 0;
+
+
+        // 0x200 is where memory starts
+        uint8_t memory[4096]={}
 
 }
