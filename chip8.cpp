780ec3f940942168def69fdbf9d690d6961b65c4
Andy <andy@Andys-MacBook-Air.local>
2025-01-18 11:47:28 -0700

    progress checkpoint

--- chip8.cpp
+++ chip8.cpp
@@ -1,8 +1,11 @@
 #include <cstdint>
-#include <cstack>
+#include <cstring>
+#include <fstream>
+#include <iostream>
 
 class chip8{
     private:
+        // I like the old logic style of c where its numeric so ill probably stick to that
         bool display[32][64] = {};
         uint8_t dataRegister[16] = {};
         //maybe input pointers to each on V0 - VF?
@@ -21,6 +24,42 @@ class chip8{
 
 
         // 0x200 is where memory starts
-        uint8_t memory[4096]={}
+        uint8_t memory[4096]={};
 
+    public:
+        bool loadProgram(std::string filePath){
+            //TODO: implement
+            // load file
+            // check that it loaded correctly
+            // loop through it byte by byte placing it starting at 200
+
+
+            std::ifstream program(filePath, std::ios::binary);
+            if (!program) {
+                std::cerr << "Failed to open file: " << filePath << std::endl;
+                return false;
+            }
+
+            //determine file length
+
+            program.seekg(0, std::ios::end);
+            size_t fileSize = program.tellg();
+            program.seekg(0, std::ios::beg);
+
+            if (fileSize > 0x1000 - 0x200){
+                std::cerr << "File too big to load into memory" << std::endl;
+                return false;
+            }
+
+            uint8_t byte;
+
+            for(int i = 0; i < fileSize ; i++){
+                program.read(reinterpret_cast<char*>(&byte), sizeof(byte));
+                memory[i + 0x200] = byte;
+            }
+
+            // Close the file
+            program.close();
+                return true;
+            }
 }
