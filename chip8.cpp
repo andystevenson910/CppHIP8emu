fb303df680e989a14b58c7d6ef956a3030909429
Andy <andy@Andys-MacBook-Air.local>
2025-01-17 17:21:56 -0700

    none

--- chip8.cpp
+++ chip8.cpp
@@ -1,5 +1,12 @@
+#include <cstdint>
 class chip8{
     private:
         bool display[32][64] = {};
-        
+        uint8_t dataRegister[16] = {};
+        //maybe input pointers to each on V0 - VF?
+
+        //12 bits wide the first nibble is unused maybe enforce that later
+        uint16_t addressRegister = 0;
+
+
 }
